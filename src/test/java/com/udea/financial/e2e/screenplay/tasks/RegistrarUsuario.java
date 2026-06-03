5a6859e880ba30fbfeaef8705432474f4dc8d30c
Reginork <danielcetina.s@gmail.com>
2026-06-03 08:27:21 -0500

    Fix issues

--- src/test/java/com/udea/financial/e2e/screenplay/tasks/RegistrarUsuario.java
+++ src/test/java/com/udea/financial/e2e/screenplay/tasks/RegistrarUsuario.java
@@ -19,6 +19,7 @@ public class RegistrarUsuario implements Task {
         this.password = password;
     }
 
+
     public static RegistrarUsuario conDatos(String email, String password) {
         return Tasks.instrumented(RegistrarUsuario.class, email, password);
     }
@@ -28,8 +29,6 @@ public class RegistrarUsuario implements Task {
         Map<String, String> bodyRequest = new HashMap<>();
         bodyRequest.put("email", email);
         bodyRequest.put("password", password);
-        // Nota: Si tu endpoint de registro en Swagger te pide más campos (como "name" o "username"),
-        // agrégalos aquí usando bodyRequest.put("campo", valor);
 
         actor.attemptsTo(
                 Post.to("/api/auth/register")
